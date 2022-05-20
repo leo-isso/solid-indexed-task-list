a0ba5d34993149af0c9663ce2a8a4df634dd6d16
Leonardo Isso <leonardo.isso@amicus.io>
2022-05-20 16:48:54 -0300

    feat: add Container component

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,14 @@
+import type { Component } from "solid-js";
+
+import type { Props } from "./types";
+import style from "./Container.module.css";
+
+const Container: Component = ({ children }: Props) => {
+  return (
+    <div class={style.wrapper}>
+      <div class={style.container}>{children}</div>
+    </div>
+  );
+};
+
+export default Container;
