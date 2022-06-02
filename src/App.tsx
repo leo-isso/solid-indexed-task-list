d46dabdf0f4bb62f52c3b29d0dae90bd1a9a2031
Leonardo Isso <leonardo.isso@amicus.io>
2022-06-02 12:47:43 -0300

    feat: add TaskList to test

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,32 @@ import type { Component } from "solid-js";
 
 import Container from "./components/Container";
 import Header from "./components/Header";
-import TaskCard from "./components/TaskCard";
+import TaskList from "./components/TaskList";
 
 const App: Component = () => {
+  const items = [
+    {
+      id: "123",
+      title: "Testing cards ma boi 0",
+      isComplete: false,
+    },
+    {
+      id: "1233",
+      title: "Testing cards ma boi 1",
+      isComplete: true,
+    },
+    {
+      id: "125",
+      title: "Testing cards ma boi 2",
+      isComplete: false,
+    },
+  ];
+
   return (
     <div>
       <Header />
       <Container>
-        <br />
-        <TaskCard
-          isCompleted={false}
-          onComplete={() => {}}
-          onDelete={() => {}}
-          title="Testing the card"
-        />
+        <TaskList items={items} />
       </Container>
     </div>
   );
