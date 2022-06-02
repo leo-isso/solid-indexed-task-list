0adddffd8d91fd55368d068864f636dee833f250
Leonardo Isso <leonardo.isso@amicus.io>
2022-06-02 13:01:15 -0300

    feat: add SortedTaskItems

--- src/components/TaskList/type.ts
+++ src/components/TaskList/type.ts
@@ -1,7 +1,12 @@
-type TaskListItems = {
+export type TaskListItems = {
   id: string;
   title: string;
   isComplete: boolean;
+};
+
+export type SortedTaskItems = {
+  todo: TaskListItems[];
+  completed: TaskListItems[];
 };
 
 export type Props = {
