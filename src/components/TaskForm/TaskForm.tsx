ddb1df1481bd4a5d6411c8a8f69e572e51ea8eff
Leonardo Isso <leonardo.isso@amicus.io>
2022-06-06 13:26:03 -0300

    feat: implement TaskForm

--- src/components/TaskForm/TaskForm.tsx
+++ src/components/TaskForm/TaskForm.tsx
@@ -1,10 +1,42 @@
-import type { Component } from "solid-js";
+import { Component, createSignal } from "solid-js";
 import { RiSystemAddLine } from "solid-icons/ri";
 
 import style from "./TaskForm.module.css";
+import TaskService from "../../services/task/task.service";
 
 const TaskForm: Component = () => {
-  return <div class={style.taskform}></div>;
+  const [getTaskTitle, setTaskTitle] = createSignal("");
+
+  const handleTitleChange = (e: Event) => {
+    setTaskTitle((e.target as HTMLInputElement).value);
+  };
+
+  const handleSubmit = async () => {
+    await TaskService.create({
+      title: getTaskTitle(),
+      isComplete: false,
+    });
+  };
+
+  return (
+    <div class={style.taskform}>
+      <div class={style.taskform_container}>
+        <form onSubmit={handleSubmit}>
+          <div class={style.taskform_form}>
+            <input
+              id="task-name"
+              value={getTaskTitle()}
+              placeholder="Add new task..."
+              onInput={(e) => handleTitleChange(e)}
+            />
+            <button type="submit">
+              <RiSystemAddLine />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default TaskForm;
