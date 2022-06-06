b33ed7c636bc400e0e3524aba174fa2e3aca903d
Leonardo Isso <leonardo.isso@amicus.io>
2022-06-06 12:17:23 -0300

    feat: add TaskForm base

---
+++ src/components/TaskForm/TaskForm.tsx
@@ -0,0 +1,10 @@
+import type { Component } from "solid-js";
+import { RiSystemAddLine } from "solid-icons/ri";
+
+import style from "./TaskForm.module.css";
+
+const TaskForm: Component = () => {
+  return <div class={style.taskform}></div>;
+};
+
+export default TaskForm;
